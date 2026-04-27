0e0bb153750aaa495f6cf9eb3b92351c749a1c54
Serhiy <seregarachuk20@gmail.com>
2026-04-27 18:02:31 +0300

    A First Component  Some State

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,8 @@
+import logoImg from '../assets./quiz-logo.png'; 
+
+export default function Header() { 
+    <header> 
+     <img src={logoImg} alt="Quiz logo" /> 
+     <h1>React Quiz</h1> 
+    </header>
+} 
