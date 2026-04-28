9edbd3fdebdbc1b79751f29864f4d95b94f6b43f
Serhiy <seregarachuk20@gmail.com>
2026-04-28 19:02:17 +0300

    Shuffling Answers Adding Quiz Logic

--- src/assets/components/Quiz.jsx
+++ src/assets/components/Quiz.jsx
@@ -1,28 +1,46 @@
-import {useState} from 'react';  
+import { useState } from 'react'; 
 import QUESTIONS from './questions.js';
+import quizIsCompleteImg from '../../assets/quiz-complete.png';
 
+export default function Quiz() { 
+    const [userAnswers, setUserAnswers] = useState([]); 
+    const activeQuestionIndex = userAnswers.length;  
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-export default function Quiz() {  
-    const [userAnswers, setUserAnswers] = useState([]);
-    const activeQuestionIndex = userAnswers.length;
-
-    function handleSelectAnswer(setUserAnswers) {
-        setUserAnswers((prevUserAnswers) =>  {
-          return [...prevUserAnswers, setUserAnswers];      
+    function handleSelectAnswer(selectedAnswer) {
+        setUserAnswers((prevUserAnswers) => {            
+            return [...prevUserAnswers, selectedAnswer]; 
         });
+    } 
+
+    if (quizIsComplete) {
+        return (
+            <div id="summary"> 
+                <img src={quizIsCompleteImg} alt='Trophy icon' />
+                <h2>Quiz Completed!</h2>
+            </div>
+        );
     }
 
-    return  <div>
-          <div id="question">
-        <h2>{QUESTIONS[activeQuestionIndex].text} </h2>    
-    <ul id="answers">
-       {QUESTIONS[activeQuestionIndex].answers.map(answers => <li key={answers} className='answer'> 
-        <button onClick={() =>  handleSelectAnswer()}>{answers}</button> 
-       </li>
-     )}     
-    </ul> 
-    </div> 
- </div>
+    
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]; 
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
+    return (
+        <div id="quiz">
+            <div id="question">
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>    
+                <ul id="answers">
+                    
+                    {shuffledAnswers.map((answer) => (
+                        <li key={answer} className='answer'>                     
+                            <button onClick={() => handleSelectAnswer(answer)}>
+                                {answer}
+                            </button> 
+                        </li>
+                    ))}     
+                </ul> 
+            </div> 
+        </div>
+    );
 }
-
-
